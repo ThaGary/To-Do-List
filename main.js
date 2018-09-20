dedeb9ddcbb1e368de8481d7b5d923522caf4cdb
ThaGary <garyanderson31591@gmail.com>
2018-09-20 15:38:40 -0600

    added Date, person, sortable table

--- main.js
+++ main.js
@@ -7,6 +7,17 @@ function submitForm(event) {
     var activity = item.value
     console.log(activity)
 
+    var person = document.getElementById('who')
+    var who = person.value
+    console.log(who)
+
+    var when = document.getElementById('event')
+    var date = when.value
+    if (date === "") {
+        date = 'Whenever'
+    }
+    console.log(date)
+
     var cost = document.getElementById('textbox')
     var price = cost.value
     console.log(price)
@@ -15,11 +26,23 @@ function submitForm(event) {
   	var tr = document.createElement('tr')
     var td = document.createElement('td')
     var td2 = document.createElement('td')
+    var td3 = document.createElement('td')
+    var td4 = document.createElement('td')
 
     tbody.appendChild(tr)
     tr.appendChild(td)
     tr.appendChild(td2)
+    tr.appendChild(td3)
+    tr.appendChild(td4)
 
-    td.innerHTML = activity
-    td2.innerHTML = '$' + price
+    td.className += " stuff"
+    td2.className += " stuff"
+    td3.className += " stuff"
+    td4.className += " stuff"
+
+    td.innerHTML = date
+    td2.innerHTML = activity
+    td3.innerHTML = '$' + price
+    td4.innerHTML = who
+
 }
